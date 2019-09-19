99fea817ef1750f72c81a1ff267ae68bfa8e9141
Mads Horn <madshorn@operamail.com>
2019-09-19 12:19:39 +0200

    Draw a square

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use nannou::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    nannou::app(model).run()
+}
+
+struct Model {
+    _window: WindowId,
+}
+
+fn model(app: &App) -> Model {
+    let _window = app
+    .new_window()
+    .with_dimensions(700, 700)
+    .with_title("Simple Neural Network")
+    .view(view)
+    .build()
+    .unwrap();
+    Model { _window }
+}
+
+fn view(app: &App, _model: &Model, frame: &Frame) {
+    draw.rect().x_y(100, 100).w_h(100, 10)
+
+
+    draw.to_frame(app, frame).unwrap();
 }
