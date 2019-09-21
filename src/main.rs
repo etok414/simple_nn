afcaa9135760c212c55223ecd237818cc138a920
Mads Horn <madshorn@operamail.com>
2019-09-21 12:28:30 +0200

    Add training data and display them

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use nannou::prelude::*;
+use std::thread::sleep;
+use std::time;
 
 fn main() {
     nannou::app(model).run()
 }
 
 struct Model {
+    training_data_in: [[f32; 4]; 16],
+    training_data_out: [[f32; 9]; 16],
     _window: WindowId,
 }
 
@@ -16,22 +20,78 @@ fn model(app: &App) -> Model {
     .view(view)
     .build()
     .unwrap();
-    Model { _window }
+
+    let training_data_in: [[f32; 4]; 16] = [
+        [0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [0.0, 0.0, 1.0, 1.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0, 1.0],
+        [0.0, 1.0, 1.0, 0.0],
+        [0.0, 1.0, 1.0, 1.0],
+        [1.0, 0.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0, 1.0],
+        [1.0, 0.0, 1.0, 0.0],
+        [1.0, 0.0, 1.0, 1.0],
+        [1.0, 1.0, 0.0, 0.0],
+        [1.0, 1.0, 0.0, 1.0],
+        [1.0, 1.0, 1.0, 0.0],
+        [1.0, 1.0, 1.0, 1.0],
+    ] ;
+
+    let training_data_out: [[f32; 9]; 16] = [
+        [0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0],
+        [0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
+        [0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0],
+        [0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0],
+        [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0],
+        [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+        [0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+        [0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0],
+        [1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+        [1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
+        [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0],
+        [1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0],
+        [1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0],
+
+
+    ] ;
+
+    Model {
+        training_data_in,
+        training_data_out,
+        _window }
 }
 
-fn view(app: &App, _model: &Model, frame: &Frame) {
+fn view(app: &App, model: &Model, frame: &Frame) {
     let draw = app.draw();
 
-    draw.rect().x_y(120.0, 180.0).w_h(50.0, 15.0).color(WHITE);
-    draw.rect().x_y(90.0, 145.0).w_h(15.0, 50.0).color(WHITE);
-    draw.rect().x_y(150.0, 145.0).w_h(15.0, 50.0).color(WHITE);
-    draw.rect().x_y(120.0, 110.0).w_h(50.0, 15.0).color(WHITE);
-    draw.rect().x_y(150.0, 75.0).w_h(15.0, 50.0).color(WHITE);
-    draw.rect().x_y(90.0, 75.0).w_h(15.0, 50.0).color(WHITE);
-    draw.rect().x_y(120.0, 40.0).w_h(50.0, 15.0).color(WHITE);
+    for n in 0..16 {
+        let tdi = model.training_data_in[n];
+        let tdu = model.training_data_out[n];
 
-    draw.rect().x_y(40.0, 75.0).w_h(15.0, 50.0).color(WHITE);
-    draw.rect().x_y(40.0, 145.0).w_h(15.0, 50.0).color(WHITE);
+        draw.ellipse().x_y(0.0, 50.0).radius(10.0).color(rgb(tdi[0], tdi[0], tdi[0]));
+        draw.ellipse().x_y(0.0, 100.0).radius(10.0).color(rgb(tdi[1], tdi[1], tdi[1]));
+        draw.ellipse().x_y(0.0, 150.0).radius(10.0).color(rgb(tdi[2], tdi[2], tdi[2]));
+        draw.ellipse().x_y(0.0, 200.0).radius(10.0).color(rgb(tdi[3], tdi[3], tdi[3]));
 
-    draw.to_frame(app, frame).unwrap();
+        draw.rect().x_y(120.0, 180.0).w_h(50.0, 15.0).color(rgb(tdu[2], tdu[2], tdu[2]));
+        draw.rect().x_y(90.0, 145.0).w_h(15.0, 50.0).color(rgb(tdu[7], tdu[7], tdu[7]));
+        draw.rect().x_y(150.0, 145.0).w_h(15.0, 50.0).color(rgb(tdu[3], tdu[3], tdu[3]));
+        draw.rect().x_y(120.0, 110.0).w_h(50.0, 15.0).color(rgb(tdu[8], tdu[8], tdu[8]));
+        draw.rect().x_y(150.0, 75.0).w_h(15.0, 50.0).color(rgb(tdu[4], tdu[4], tdu[4]));
+        draw.rect().x_y(90.0, 75.0).w_h(15.0, 50.0).color(rgb(tdu[6], tdu[6], tdu[6]));
+        draw.rect().x_y(120.0, 40.0).w_h(50.0, 15.0).color(rgb(tdu[5], tdu[5], tdu[5]));
+
+        draw.rect().x_y(40.0, 75.0).w_h(15.0, 70.0).color(rgb(tdi[0], tdi[0], tdi[0]));
+        draw.rect().x_y(40.0, 145.0).w_h(15.0, 70.0).color(rgb(tdi[1], tdi[1], tdi[1]));
+
+        draw.to_frame(app, frame).unwrap();
+
+        sleep(time::Duration::new(1, 0));
+    }
 }
