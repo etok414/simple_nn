236455b7e6548b1c0d68553070b95d02c861533a
HappyDustbunny <madshorn@operamail.com>
2019-09-23 21:52:19 +0200

    7 segment looks ok

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ use std::thread::sleep;
 use std::time;
 
 fn main() {
-    nannou::app(model).run()
+    nannou::app(model)
+        .update(update)
+        .run()
 }
 
 struct Model {
     training_data_in: [[f32; 4]; 16],
     training_data_out: [[f32; 9]; 16],
+    time: usize,
     _window: WindowId,
 }
 
@@ -51,47 +54,59 @@ fn model(app: &App) -> Model {
         [0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0],
         [0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
         [0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0],
-        [1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0],
         [1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0],
         [1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0],
         [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0],
         [1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0],
         [1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0],
-
-
     ] ;
+
+    let time = 0;
 
     Model {
         training_data_in,
         training_data_out,
+        time,
         _window }
 }
 
+fn update(_app: &App, model: &mut Model, _update: Update) {
+
+    model.time += 1;
+
+    if model.time > 15 {
+        model.time = 0;
+    }
+
+    sleep(time::Duration::new(1, 0));
+
+}
+
 fn view(app: &App, model: &Model, frame: &Frame) {
+
     let draw = app.draw();
 
-    for n in 0..16 {
-        let tdi = model.training_data_in[n];
-        let tdu = model.training_data_out[n];
+    let t = model.time;
 
-        draw.ellipse().x_y(0.0, 50.0).radius(10.0).color(rgb(tdi[0], tdi[0], tdi[0]));
-        draw.ellipse().x_y(0.0, 100.0).radius(10.0).color(rgb(tdi[1], tdi[1], tdi[1]));
-        draw.ellipse().x_y(0.0, 150.0).radius(10.0).color(rgb(tdi[2], tdi[2], tdi[2]));
-        draw.ellipse().x_y(0.0, 200.0).radius(10.0).color(rgb(tdi[3], tdi[3], tdi[3]));
+    let tdi = model.training_data_in[t];
+    let tdu = model.training_data_out[t];
 
-        draw.rect().x_y(120.0, 180.0).w_h(50.0, 15.0).color(rgb(tdu[2], tdu[2], tdu[2]));
-        draw.rect().x_y(90.0, 145.0).w_h(15.0, 50.0).color(rgb(tdu[7], tdu[7], tdu[7]));
-        draw.rect().x_y(150.0, 145.0).w_h(15.0, 50.0).color(rgb(tdu[3], tdu[3], tdu[3]));
-        draw.rect().x_y(120.0, 110.0).w_h(50.0, 15.0).color(rgb(tdu[8], tdu[8], tdu[8]));
-        draw.rect().x_y(150.0, 75.0).w_h(15.0, 50.0).color(rgb(tdu[4], tdu[4], tdu[4]));
-        draw.rect().x_y(90.0, 75.0).w_h(15.0, 50.0).color(rgb(tdu[6], tdu[6], tdu[6]));
-        draw.rect().x_y(120.0, 40.0).w_h(50.0, 15.0).color(rgb(tdu[5], tdu[5], tdu[5]));
+    draw.ellipse().x_y(-100.0, 5.0).radius(10.0).color(rgb(tdi[0], tdi[0], tdi[0]));
+    draw.ellipse().x_y(-100.0, 62.0).radius(10.0).color(rgb(tdi[1], tdi[1], tdi[1]));
+    draw.ellipse().x_y(-100.0, 123.0).radius(10.0).color(rgb(tdi[2], tdi[2], tdi[2]));
+    draw.ellipse().x_y(-100.0, 192.0).radius(10.0).color(rgb(tdi[3], tdi[3], tdi[3]));
 
-        draw.rect().x_y(40.0, 75.0).w_h(15.0, 70.0).color(rgb(tdi[0], tdi[0], tdi[0]));
-        draw.rect().x_y(40.0, 145.0).w_h(15.0, 70.0).color(rgb(tdi[1], tdi[1], tdi[1]));
+    draw.rect().x_y(50.0, 50.0).w_h(16.0, 84.0).color(rgb(tdu[0], tdu[0], tdu[0]));
+    draw.rect().x_y(50.0, 150.0).w_h(16.0, 84.0).color(rgb(tdu[1], tdu[1], tdu[1]));
 
-        draw.to_frame(app, frame).unwrap();
+    draw.rect().x_y(135.0, 192.0).w_h(44.0, 16.0).color(rgb(tdu[2], tdu[2], tdu[2]));
+    draw.rect().x_y(165.0, 150.0).w_h(16.0, 84.0).color(rgb(tdu[3], tdu[3], tdu[3]));
+    draw.rect().x_y(165.0, 50.0).w_h(16.0, 84.0).color(rgb(tdu[4], tdu[4], tdu[4]));
+    draw.rect().x_y(135.0, 8.0).w_h(44.0, 16.0).color(rgb(tdu[5], tdu[5], tdu[5]));
+    draw.rect().x_y(100.0, 50.0).w_h(16.0, 84.0).color(rgb(tdu[6], tdu[6], tdu[6]));
+    draw.rect().x_y(100.0, 150.0).w_h(16.0, 84.0).color(rgb(tdu[7], tdu[7], tdu[7]));
+    draw.rect().x_y(135.0, 100.0).w_h(44.0, 16.0).color(rgb(tdu[8], tdu[8], tdu[8]));
 
-        sleep(time::Duration::new(1, 0));
-    }
+    draw.to_frame(app, frame).unwrap();
 }
