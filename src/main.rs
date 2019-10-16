41581cc79d6830aa32fec9fddee7d10f2fe2a697
Toke Horn Brorholt <etok414@gmail.com>
2019-10-16 11:38:43 +0200

    Added the systems for adjusting weights and biases

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,15 @@ struct Model {
 struct Node {
     bias: f32,
     weights: Vec<f32>,
+    bias_adjust: Option<f32>,
+    weight_adjusts: Vec<f32>,
 }
 
 impl Node {
     pub fn new(number_of_weights: usize) -> Node {
         // Should probably be random.
-        Node {bias: 0.0, weights: vec![0.0; number_of_weights]}
+        Node {bias: 0.0, weights: vec![0.0; number_of_weights],
+              bias_adjust: None, weight_adjusts: Vec::new()}
     }
 
     pub fn calculate(&self, previous_layer:&Vec<f32>) -> f32 {
@@ -43,6 +46,53 @@ impl Node {
         }
         norm_value
     }
+
+    pub fn find_adjusts(&mut self, previous_layer:&Vec<f32>, personal_value:f32, desired_value:f32, next_layer: &Layer) {
+        if self.bias_adjust.is_some() {
+            panic!("The bias adjust contained a value before adjusts were found");
+        }
+        if self.weight_adjusts.len() != 0 {
+            panic!("The weight adjusts contained {} value(s) before adjusts were found", self.weight_adjusts.len());
+        }
+        let previous_layer_len = previous_layer.len();
+        if self.weights.len() != previous_layer_len {
+            panic!("The number of weights ({}) doesn't match the number of values ({})", self.weights.len(), previous_layer_len);
+        }
+        let mut delta = 0.0;
+        if next_layer.nodes.len() == 0 {
+            delta = (personal_value - desired_value) * personal_value * (1.0 - personal_value)
+        } else {
+            let next_len = next_layer.nodes.len();
+            for num in 0..next_len {
+                delta += next_layer.nodes[num].bias_adjust.unwrap();
+            }
+            delta *= personal_value * (1.0 - personal_value);
+        }
+        self.bias_adjust = Some(delta);
+        for pos_num in 0..previous_layer_len {
+            self.weight_adjusts.push(delta * previous_layer[pos_num]);
+        }
+    }
+
+    // fn find_weight_adjust(previous_node:f32, personal_value:f32, desired_value:f32) -> f32 {
+    //     (personal_value - desired_value) * personal_value * (1.0 + personal_value) * previous_node
+    // }
+
+    pub fn adjust(&mut self, learning_rate: f32) {
+        if self.bias_adjust.is_none() {
+            panic!{"Bias adjust is missing"}
+        }
+        let weight_count = self.weights.len();
+        if self.weight_adjusts.len() != weight_count {
+            panic!("The number of weight adjusts ({}) doesn't match the number of weights ({})", self.weight_adjusts.len(), weight_count);
+        }
+        self.bias -= self.bias_adjust.unwrap() * learning_rate;
+        self.bias_adjust = None;
+        for num in 0..weight_count {
+            self.weights[num] -= self.weight_adjusts[num] * learning_rate;
+        }
+        self.weight_adjusts = Vec::new();
+    }
 }
 
 struct Layer {
@@ -57,6 +107,20 @@ impl Layer {
             values.push(self.nodes[node_num].calculate(previous_layer));
         }
         values
+    }
+
+    pub fn find_adjusts(&mut self, previous_layer:&Vec<f32>, values:&Vec<f32>, desired_values:&Vec<f32>, next_layer: &Layer) {
+        let node_count = self.nodes.len();
+        for node_num in 0..node_count {
+            self.nodes[node_num].find_adjusts(previous_layer, values[node_num], desired_values[node_num], next_layer);
+        }
+    }
+
+    pub fn adjust(&mut self, learning_rate:f32) {
+        let node_count = self.nodes.len();
+        for node_num in 0..node_count {
+            self.nodes[node_num].adjust(learning_rate)
+        }
     }
 }
 
