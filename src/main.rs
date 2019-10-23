8a596af0720f7bab5d9ff1756bc26e55259c9972
Toke Horn Brorholt <etok414@gmail.com>
2019-10-23 13:41:35 +0200

    Made my code more readable. Fixed minor display issues.

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,7 @@ struct Model {
 
 #[derive(Clone)]
 struct Node {
+// A node/neuron's bias and the weights of its connections to the previous layer.
     bias: f32,
     weights: Vec<f32>,
     bias_adjust: Option<f32>,
@@ -33,6 +34,7 @@ struct Node {
 
 impl Node {
     pub fn new(number_of_weights: usize) -> Node {
+    //Generates a new node with a random bias and random weights.
         let mut rng = rand::thread_rng();
 
         let mut init_weights = vec![0.0; number_of_weights];
@@ -44,20 +46,23 @@ impl Node {
         let x: f32 = rng.gen();  // Random number in the interval [0; 1[
         let bias = 2.0 * x  - 1.0;  // The initial weights will be in [-1; 1[
 
-        Node {bias: bias,
-              weights: init_weights, // vec![0.0; number_of_weights],
-              bias_adjust: None,
-              weight_adjusts: Vec::new()}
+        Node {
+            bias: bias,
+            weights: init_weights, // vec![0.0; number_of_weights],
+            bias_adjust: None,
+            weight_adjusts: Vec::new()
+        }
     }
 
-    pub fn calculate(&self, previous_layer:&Vec<f32>) -> f32 {
+    pub fn calculate(&self, previous_layer_values:&Vec<f32>) -> f32 {
+    //Calculates the value of the node based on the values of the previous layer and the node's bias and weights.
         let mut value = self.bias;
-        let previous_layer_len = previous_layer.len();
+        let previous_layer_len = previous_layer_values.len();
         if self.weights.len() != previous_layer_len {
             panic!("The number of weights ({}) doesn't match the number of values ({})", self.weights.len(), previous_layer_len);
         }
         for pos_num in 0..previous_layer_len {
-            value += previous_layer[pos_num] * self.weights[pos_num];
+            value += previous_layer_values[pos_num] * self.weights[pos_num];
         }
         let norm_value = 1.0 / (1.0 + (-value).exp());
         if norm_value < 0.0 || norm_value > 1.0 {
@@ -66,30 +71,33 @@ impl Node {
         norm_value
     }
 
-    pub fn find_adjusts(&mut self, previous_layer:&Vec<f32>, personal_value:f32, desired_value:f32, next_layer: &Layer) {
+    pub fn find_adjusts(&mut self, previous_layer_values:&Vec<f32>, personal_value:f32, desired_value:f32, next_layer: &Layer) {
+    //Finds out how the node's bias and weights should be adjusted based on either a known desired value or how the next layer is set to be adjusted.
+    //If it receives a next_layer with any nodes in it, it uses the bias_adjusts of the nodes in that layer that connect with it along with its personal_value to find its own bias_adjust.
+    //Otherwise, it finds its bias_adjust from its personal_value and a specified desired_value.
+    //It then finds its weight_adjusts from its bias_adjust and previous_layer_values.
         if self.bias_adjust.is_some() {
             panic!("The bias adjust contained a value before adjusts were found");
         }
         if self.weight_adjusts.len() != 0 {
             panic!("The weight adjusts contained {} value(s) before adjusts were found", self.weight_adjusts.len());
         }
-        let previous_layer_len = previous_layer.len();
+        let previous_layer_len = previous_layer_values.len();
         if self.weights.len() != previous_layer_len {
             panic!("The number of weights ({}) doesn't match the number of values ({})", self.weights.len(), previous_layer_len);
         }
         let mut delta = 0.0;
-        if next_layer.nodes.len() == 0 {
+        if next_layer.node_count == 0 {
             delta = (personal_value - desired_value) * personal_value * (1.0 - personal_value)
         } else {
-            let next_len = next_layer.nodes.len();
-            for num in 0..next_len {
-                delta += next_layer.nodes[num].bias_adjust.unwrap() * next_layer.nodes[num].weights[desired_value as usize];
+            for num in 0..next_layer.node_count {
+                delta += next_layer.nodes[num].bias_adjust.unwrap() * next_layer.nodes[num].weights[desired_value as usize]; //If there is a next_layer, desired_value tracks which connections in it lead to the node.
             }
             delta *= personal_value * (1.0 - personal_value);
         }
         self.bias_adjust = Some(delta);
         for pos_num in 0..previous_layer_len {
-            self.weight_adjusts.push(delta * previous_layer[pos_num]);
+            self.weight_adjusts.push(delta * previous_layer_values[pos_num]);
         }
     }
 
@@ -98,6 +106,7 @@ impl Node {
     // }
 
     pub fn adjust(&mut self, learning_rate: f32) {
+    //Changes the weights and biases after all nodes have found how they're supposed to be adjusted.
         if self.bias_adjust.is_none() {
             panic!{"Bias adjust is missing"}
         }
@@ -116,44 +125,52 @@ impl Node {
 
 #[derive(Clone)]
 struct Layer {
+//A layer of nodes, their biases, and the weights of their connections to the previous layer.
     nodes: Vec<Node>,
+    node_count: usize, //Should be equal to nodes.len() and shouldn't change.
 }
 
 impl Layer {
-    pub fn new(number_of_weights: usize, number_of_nodes: usize) -> Layer {
-        let mut layer = Layer{nodes:Vec::new()};
+    pub fn new(previous_layer_nodes: usize, number_of_nodes: usize) -> Layer {
+    //Generates a layer of nodes, each with a random bias and a number of random weights equal to the number of nodes in the previous layer.
+        let mut nodes = Vec::new();
         for _ in 0..number_of_nodes {
-            layer.nodes.push(Node::new(number_of_weights));
+            nodes.push(Node::new(previous_layer_nodes));
         }
-        layer
+        let node_count = nodes.len();
+        Layer {
+            nodes: nodes,
+            node_count: node_count,
+        }
     }
 
-    pub fn calculate(&self, previous_layer:&Vec<f32>) -> Vec<f32> {
+    pub fn calculate(&self, previous_layer_values:&Vec<f32>) -> Vec<f32> {
+    //Calculates the values of the nodes based on the values of the previous layer and the nodes' weights and biases.
         let mut values = Vec::new();
-        let node_count = self.nodes.len();
-        for node_num in 0..node_count {
-            values.push(self.nodes[node_num].calculate(previous_layer));
+        for node_num in 0..self.node_count {
+            values.push(self.nodes[node_num].calculate(previous_layer_values));
         }
         values
     }
 
-    pub fn find_adjusts(&mut self, previous_layer:&Vec<f32>, values:&Vec<f32>, desired_values:&Vec<f32>, next_layer: Layer) {
-        let node_count = self.nodes.len();
-        for node_num in 0..node_count {
-            self.nodes[node_num].find_adjusts(previous_layer,
-                                              values[node_num],
-                                              match desired_values.len() {
-                                                  0 => node_num as f32,
-                                                  _ => desired_values[node_num],
-                                              },
-                                              &next_layer
+    pub fn find_adjusts(&mut self, previous_layer_values:&Vec<f32>, values:&Vec<f32>, desired_values:&Vec<f32>, next_layer: &Layer) {
+    //Finds out how the nodes' weights and biases should be adjusted, based either on a list of desired values, or how the next layer is set to be adjusted.
+    //The function does this by calling find_adjusts for each node.
+        for node_num in 0..self.node_count {
+            self.nodes[node_num].find_adjusts(
+                                              previous_layer_values,
+                                              values[node_num], //personal_value, is used to track the calculated value of the relevant node.
+                                              if next_layer.node_count > 0 {node_num as f32} //If the next layer contains anything, desired_value is used to track the position of the relevant node.
+                                                else {desired_values[node_num]}, //Otherwise, desired_value is used to track the desired value.
+                                              next_layer
                                               );
         }
     }
 
     pub fn adjust(&mut self, learning_rate:f32) {
-        let node_count = self.nodes.len();
-        for node_num in 0..node_count {
+    //Calls the adjust function of every node in the layer, which does the following for the node:
+    //Changes the weights and biases after all nodes have found how they're supposed to be adjusted.
+        for node_num in 0..self.node_count {
             self.nodes[node_num].adjust(learning_rate)
         }
     }
@@ -172,13 +189,13 @@ fn calculate(model: &Model) -> Vec<Vec<f32>> {
 fn find_make_adjust(model: &mut Model) {
     let values = calculate(&model);
     let layer_count = model.layers.len();
-    model.layers[layer_count-1].find_adjusts(&values[layer_count-2], &values[layer_count-1], &model.training_data_out[model.relevant_data].to_vec(), Layer::new(0, 0));
+    model.layers[layer_count-1].find_adjusts(&values[layer_count-2], &values[layer_count-1], &model.training_data_out[model.relevant_data].to_vec(), &Layer::new(0, 0));
     for num in (1..layer_count-1).rev() {
         let next_layer = model.layers[num+1].clone();
-        model.layers[num].find_adjusts(&values[num-1], &values[num], &Vec::new(), next_layer);
+        model.layers[num].find_adjusts(&values[num-1], &values[num], &Vec::new(), &next_layer);
     }
     let next_layer = model.layers[1].clone();
-    model.layers[0].find_adjusts(&model.training_data_in[model.relevant_data].to_vec(), &values[0], &Vec::new(), next_layer);
+    model.layers[0].find_adjusts(&model.training_data_in[model.relevant_data].to_vec(), &values[0], &Vec::new(), &next_layer);
     for num in 0..layer_count {
         model.layers[num].adjust(model.learning_rate);
     }
@@ -313,32 +330,16 @@ fn draw_results(model: &Model, draw: &nannou::app::Draw) {
     draw.ellipse().x_y(-80.0, 165.0).radius(6.0).color(rgb(1.0, 1.0, 1.0));
     draw.ellipse().x_y(-80.0, 192.0).radius(6.0).color(rgb(1.0, 1.0, 1.0));
 
-    // 1 part of the 7 segment display
-    // draw.polygon().color(rgb(tdu[1], tdu[1], tdu[1])).points(vec!(pt2(106.0 + 67.0, 184.0), pt2(112.0 + 67.0, 178.0),pt2(112.0 + 67.0, 112.0), pt2(106.0 + 67.0, 103.0), pt2(96.0 + 67.0, 109.0), pt2(96.0 + 67.0, 176.0))); // B
-    // draw.rect().x_y(50.0, 150.0).w_h(16.0, 84.0).color(rgb(tdu[1], tdu[1], tdu[1])); // B
+    // "1" part of the 7 segment display
     draw.polygon().color(rgb(tdu[1], tdu[1], tdu[1])).points(vec!(pt2(106.0 - 62.0, 184.0), pt2(112.0 - 62.0, 178.0),pt2(112.0 - 62.0, 112.0), pt2(106.0 - 62.0, 103.0), pt2(96.0 - 62.0, 109.0), pt2(96.0 - 62.0, 176.0))); // B
-    // draw.tri().points((42.0, 192.0), (58.0, 192.0), (58.0, 200.0)).color(rgb(tdu[1], tdu[1], tdu[1])); // B corner
-    // draw.tri().points((42.0, 108.0), (58.0, 108.0), (58.0, 100.0)).color(rgb(tdu[1], tdu[1], tdu[1])); // B corner
-    // draw.rect().x_y(50.0, 50.0).w_h(16.0, 84.0).color(rgb(tdu[0], tdu[0], tdu[0])); // C
     draw.polygon().color(rgb(tdu[0], tdu[0], tdu[0])).points(vec!(pt2(106.0 - 62.0, 184.0 - 85.0), pt2(112.0 - 62.0, 178.0 - 85.0),pt2(112.0 - 62.0, 112.0 - 85.0), pt2(106.0 - 62.0, 103.0 - 85.0), pt2(96.0 - 62.0, 109.0 - 85.0), pt2(96.0 - 62.0, 176.0 - 85.0))); // C
 
-    // 8 part of the 7 segment display
-    // draw.rect().x_y(135.0, 192.0).w_h(44.0, 16.0).color(rgb(tdu[2], tdu[2], tdu[2])); // A
+    // "8" part of the 7 segment display
     draw.polygon().color(rgb(tdu[2], tdu[2], tdu[2])).points(vec!(pt2(108.0, 190.0), pt2(113.0, 197.0),pt2(163.0, 197.0), pt2(172.0, 189.0), pt2(161.0, 182.0), pt2(115.0, 182.0))); // A
-    // draw.tri().points((157.0, 200.0), (165.0, 200.0), (157.0, 184.0)).color(rgb(tdu[2], tdu[2], tdu[2])); // A corner
-    // draw.tri().points((113.0, 200.0), (105.0, 200.0), (113.0, 184.0)).color(rgb(tdu[2], tdu[2], tdu[2])); // A corner
-    // draw.rect().x_y(165.0, 150.0).w_h(16.0, 84.0).color(rgb(tdu[3], tdu[3], tdu[3])); // B
     draw.polygon().color(rgb(tdu[3], tdu[3], tdu[3])).points(vec!(pt2(106.0 + 67.0, 184.0), pt2(112.0 + 67.0, 178.0),pt2(112.0 + 67.0, 112.0), pt2(106.0 + 67.0, 103.0), pt2(96.0 + 67.0, 109.0), pt2(96.0 + 67.0, 176.0))); // B
-    // draw.tri().points((157.0, 192.0), (173.0, 192.0), (173.0, 200.0)).color(rgb(tdu[3], tdu[3], tdu[3])); // B corner
-    // draw.tri().points((157.0, 108.0), (173.0, 108.0), (173.0, 100.0)).color(rgb(tdu[3], tdu[3], tdu[3])); // B corner
-    // draw.rect().x_y(165.0, 50.0).w_h(16.0, 84.0).color(rgb(tdu[4], tdu[4], tdu[4])); // C
     draw.polygon().color(rgb(tdu[4], tdu[4], tdu[4])).points(vec!(pt2(106.0 + 67.0, 184.0 - 85.0), pt2(112.0 + 67.0, 178.0 - 85.0),pt2(112.0 + 67.0, 112.0 - 85.0), pt2(106.0 + 67.0, 103.0 - 85.0), pt2(96.0 + 67.0, 109.0 - 85.0), pt2(96.0 + 67.0, 176.0 - 85.0))); // C
-    // draw.rect().x_y(135.0, 8.0).w_h(44.0, 16.0).color(rgb(tdu[5], tdu[5], tdu[5])); // D
-    draw.polygon().color(rgb(tdu[2], tdu[2], tdu[2])).points(vec!(pt2(108.0, 190.0 - 176.0), pt2(113.0, 197.0 - 176.0),pt2(163.0, 197.0 - 176.0), pt2(172.0, 189.0 - 176.0), pt2(161.0, 182.0 - 176.0), pt2(115.0, 182.0 - 176.0))); // D
-    // draw.rect().x_y(100.0, 50.0).w_h(16.0, 84.0).color(rgb(tdu[6], tdu[6], tdu[6])); // E
+    draw.polygon().color(rgb(tdu[5], tdu[5], tdu[5])).points(vec!(pt2(108.0, 190.0 - 176.0), pt2(113.0, 197.0 - 176.0),pt2(163.0, 197.0 - 176.0), pt2(172.0, 189.0 - 176.0), pt2(161.0, 182.0 - 176.0), pt2(115.0, 182.0 - 176.0))); // D
     draw.polygon().color(rgb(tdu[6], tdu[6], tdu[6])).points(vec!(pt2(106.0, 184.0 - 85.0), pt2(112.0, 178.0 - 85.0),pt2(112.0, 112.0 - 85.0), pt2(106.0, 103.0 - 85.0), pt2(96.0, 109.0 - 85.0), pt2(96.0, 176.0 - 85.0))); // E
-    // draw.rect().x_y(100.0, 150.0).w_h(16.0, 84.0).color(rgb(tdu[7], tdu[7], tdu[7])); // F
     draw.polygon().color(rgb(tdu[7], tdu[7], tdu[7])).points(vec!(pt2(106.0, 184.0), pt2(112.0, 178.0),pt2(112.0, 112.0), pt2(106.0, 103.0), pt2(96.0, 109.0), pt2(96.0, 176.0))); // F
-    // draw.rect().x_y(135.0, 100.0).w_h(44.0, 16.0).color(rgb(tdu[8], tdu[8], tdu[8])); // G
-    draw.polygon().color(rgb(tdu[2], tdu[2], tdu[2])).points(vec!(pt2(108.0, 190.0 - 90.0), pt2(113.0, 197.0 - 90.0),pt2(163.0, 197.0 - 90.0), pt2(172.0, 189.0 - 90.0), pt2(161.0, 182.0 - 90.0), pt2(115.0, 182.0 - 90.0))); // G
+    draw.polygon().color(rgb(tdu[8], tdu[8], tdu[8])).points(vec!(pt2(108.0, 190.0 - 90.0), pt2(113.0, 197.0 - 90.0),pt2(163.0, 197.0 - 90.0), pt2(172.0, 189.0 - 90.0), pt2(161.0, 182.0 - 90.0), pt2(115.0, 182.0 - 90.0))); // G
 }
